f02af914637b7d38425a89139cf9513d5862a951
Jessica <jessica_is2@hotmail.com>
2020-07-02 13:10:50 -0300

    Configuração para Monitorar Erros com Sentry

--- pypro/settings.py
+++ pypro/settings.py
@@ -17,6 +17,9 @@ from functools import partial
 import dj_database_url
 from decouple import config
 from decouple import Csv
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -175,3 +178,8 @@ COLLECTFAST_ENABLED = False
 #     INSTALLED_APPS.append('s3_folder_storage')
 #     INSTALLED_APPS.append('storages')
 #
+
+SENTRY_DSN = config('SENTRY_DSN', default=None)
+
+if SENTRY_DSN:
+    sentry_sdk.init(dsn=SENTRY_DSN, integrations=[DjangoIntegration()])
